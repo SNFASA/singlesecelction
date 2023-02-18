ff357e47e6b58f54cfe83c6f5fee45a984d8210d
SYED NABIL <89401427+SNFASA@users.noreply.github.com>
2023-02-18 15:33:39 +0800

    Add files via upload

---
+++ singleselection.java
@@ -0,0 +1,17 @@
+import java.util.Scanner ;
+public class singleselection{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your modul : ");
+        int modul = sc.nextInt();
+        if (modul == 1)
+            System.out.println("The student is taking Biology");
+       
+        
+        
+        
+       
+        
+    
+    }
+}
